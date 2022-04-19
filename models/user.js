bbedfc896a0cfa17984522a43f38f60cb6144dd4
ignasiusberneo <ignasiusberneo95@gmail.com>
2022-04-19 21:27:44 +0700

    seeding

---
+++ models/user.js
@@ -0,0 +1,55 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsToMany(models.Drama, { through: "Watchlists" });
+      this.belongsToMany(models.Drama, { through: "Comments" });
+    }
+  }
+  User.init(
+    {
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: { msg: "Email already exists" },
+        validate: {
+          isEmail: { msg: "Invalid email format" },
+          notNull: { msg: "Email is required" },
+          notEmpty: { msg: "Email is required" },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: {
+            args: [8, 20],
+            msg: "Password must be 8-20 characters",
+          },
+          notNull: { msg: "Password is required" },
+          notEmpty: { msg: "Password is required" },
+        },
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: { msg: "Username is required" },
+          notEmpty: { msg: "Username is required" },
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+  return User;
+};
