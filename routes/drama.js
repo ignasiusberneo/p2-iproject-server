e87e8e5ad6f56002bf81343fb193169cebbeef9b
ignasiusberneo <ignasiusberneo95@gmail.com>
2022-04-21 10:27:13 +0700

    Final Server

--- routes/drama.js
+++ routes/drama.js
@@ -4,6 +4,7 @@ const dramaController = require("../controllers/dramaController");
 const authentication = require("../middlewares/authentication");
 
 router.get("/", dramaController.getDramas);
+router.post("/omdb", dramaController.getOmdb);
 
 router.get("/watchlist", authentication, dramaController.getWatchlist);
 router.get("/:dramaId", dramaController.getDramaById);
